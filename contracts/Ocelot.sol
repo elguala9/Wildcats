4c35c9f46f89504e7b04af1ba4f78653d587c2d7
"Luca" <"luca.gualandi96@gmail.com">
2022-04-27 16:21:24 +0200

    No recipient in mint.

--- contracts/Ocelot.sol
+++ contracts/Ocelot.sol
@@ -3,16 +3,16 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
-contract Ocelot is ERC721URIStorage, Ownable {
+
+contract Ocelot is ERC721, Ownable {
     // Costants
     uint256 constant NFT_PRICE = 80000000000000; //0.00008 ETH
     uint16 constant MAX_NORMAL_NFT = 510;
     uint16 constant MAX_CUSTOM_NFT = 110;
     // Variables of the contract
-    uint256  private _mintedNFTs = 0;
     uint256 private _normalNFTs = 0;
     uint256 private _customNFTs = 0;
     uint16 private _availableNFTs = 0;
@@ -33,43 +33,40 @@ contract Ocelot is ERC721URIStorage, Ownable {
         return  address(this).balance;
     }
 
-    // mint a token wihtout setting the URI 
-    function mintOcelot(address recipient) public payable {
+    
+
+    // mint the normal NFT
+    function mintOcelot() public payable{
         require(_normalNFTs < MAX_NORMAL_NFT, "NFTs are finished");
         require(_availableNFTs > 0, "No NFTs are available");
         require(NFT_PRICE <= msg.value, "The Ether sent is not enough");
-        _safeMint(recipient, _normalNFTs);
+        _safeMint(msg.sender, _normalNFTs);
         _normalNFTs++;
-        _mintedNFTs++;
-    }
-
-    // for lazy people that do not want to pass the address
-    function mintOcelot() public payable{
-        mintOcelot(msg.sender);
     }
 
     // 0 - 199 token ID: custom rare 
-    function mintCustomOcelot(address recipient) public onlyOwner{
-        require(_customNFTs < MAX_CUSTOM_NFT, "NFTs are finished");
-        _safeMint(recipient, _customNFTs);
-        _customNFTs++;
-        _mintedNFTs++;
+    function mintCustomOcelot(address recipient) internal onlyOwner{
+        
         
     }
-    // for lazy people that do not want to pass the address
+    // mint the custom Ocelot
     function mintCustomOcelot() public onlyOwner{
-        return mintCustomOcelot(msg.sender);
+        require(_customNFTs < MAX_CUSTOM_NFT, "NFTs are finished");
+        _safeMint(msg.sender, _customNFTs);
+        _customNFTs++;
     }
     
+    
+    // I need this to start the sale
+    function setAvailbleNFTs(uint16 supply) public onlyOwner{
+        _availableNFTs += supply;
+    }
 
-    // 
-    function setUriNFT(uint256 tokenId, string calldata tokenURI) public onlyOwner {
-        _setTokenURI(tokenId, tokenURI);
+    function setBaseUri(string calldata base_uri) public onlyOwner{
+        _baseUri = base_uri;
     }
-    // how much NFTs are on the chain
-    function circulation() public view returns (uint256) {
-        return _mintedNFTs;
-    }
+
+    //GETTER
     // how much normal NFTs are on the chain
     function circulationNormal() public view returns (uint256) {
         return _normalNFTs - MAX_CUSTOM_NFT;
@@ -78,17 +75,11 @@ contract Ocelot is ERC721URIStorage, Ownable {
     function circulationCustom() public view returns (uint256) {
         return _customNFTs;
     }
-    // I need this to start the sale
-    function setAvailbleNFTs(uint16 supply) public {
-        _availableNFTs += supply;
+    function getPrice() public pure returns (uint256) {
+        return NFT_PRICE;
     }
 
-    function setBaseUri(string calldata baseUri) public{
-        _baseUri = baseUri;
-    }
-        
-    // OVERRIDE
-    function _baseURI() override internal view virtual returns (string memory) {     
-        return _baseUri;
+    function availableNFTs() public view returns (uint16) {
+        return _availableNFTs;
     }
 }
